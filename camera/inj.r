5a32f73908de547129e655a4648e9b33bb8f24a6
joozooclub464 <86102525+joozooclub464@users.noreply.github.com>
2022-08-02 19:45:19 +0900

    Create inj.r

---
+++ camera/inj.r
@@ -0,0 +1,69 @@
+#### 독립표본 t-test#
+
+#01. 데이터 불러오기
+ca <- read.csv("./data.csv",
+               header = TRUE,
+               na.strings = ".")
+str(ca)
+
+ca$ca_yn <- factor(ca$ca_yn,
+                   levels = c(0,1),
+                   labels = c("무","유"))
+str(ca)
+
+attach(ca) #객체연결 (attach ->detach)
+
+#02. 기본통계치 확인 : summary or describe(psych 패키지 이용)
+#tapply(total, ca_yn,summary)
+
+library(psych)
+describeBy(inj, ca_yn, mat = T)
+
+#03. 그래프 그리기(박스그래프, 히스토그램)
+opar <- par(no.readonly = TRUE)
+layout(matrix(c(1,1,2,3),2,2, byrow = TRUE)) #화면분할
+boxplot(inj ~ ca_yn)
+hist(inj[ca_yn == "무"])
+hist(inj[ca_yn == "유"])
+par(opar)
+
+#04. 통계분석
+# 등분산 검정
+var.test(inj ~ ca_yn, data = ca) #0%가 동질하다 ie) 이분산임.
+
+t.test(inj ~ ca_yn,
+       data = ca,
+       alternative = c("two.sided"),
+       var.equal = FALSE,
+       conf.level = 0.95)
+detach(ca)
+
+
+
+#05. 통계결과 그래프
+mu=41.688312
+se=3.04
+data <- rnorm(1000, mu,se) #
+data <- sort(data)
+plot(data, dnorm(data,mu,se), 
+     col = "blue", 
+     type = 'l',
+     main = "무인카메라 설치 여부와 경상자수의 관계",
+     xlim = c(0,50),   #
+     ylim = c(0,2))   #
+abline(v=mu, col = "blue",lty = 3)
+
+par(new=T) 
+
+#
+mu=3.032609
+se=0.30
+
+data <- rnorm(1000, mu,se) #
+data <- sort(data)
+plot(data, dnorm(data,mu,se), 
+     col = 'red', 
+     type = 'l',
+     xlim = c(0,50),   #
+     ylim = c(0,2))   #
+abline(v=mu, col = "red",lty = 3)
